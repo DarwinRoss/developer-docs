8f5af4418694a888434371ff4aea2c00f2d18d02
cayanlarkin <mlarkin@cayan.com>
2018-03-23 14:15:42 +0000

    StyleCop validation

--- examples/Genius/csharp/GeniusSample/GeniusSample/Program.cs
+++ examples/Genius/csharp/GeniusSample/GeniusSample/Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using GeniusSample.TransportWeb;
-using System.Net;
 using System.IO;
+using System.Net;
 using System.Xml.Serialization;
+using GeniusSample.TransportWeb;
 
 namespace GeniusSample
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //Declare credentials to be used with the Stage Transaction Request
+            // Declare credentials to be used with the Stage Transaction Request
             string credentialsName = "TEST MERCHANT";
             string credentialsSiteID = "XXXXXXXX";
             string credentialsKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX";
             string ipAddress = "192.168.0.123";
 
-            //Build Transport request details
+            // Build Transport request details
             Console.WriteLine("Staging Transaction{0}", Environment.NewLine);
             TransportServiceSoapClient transportServiceSoapClient = new TransportServiceSoapClient();
             TransportRequest transportRequest = new TransportRequest
@@ -34,30 +34,30 @@ namespace GeniusSample
                 ForceDuplicate = true,
             };
 
-            //Stage Transaction
+            // Stage Transaction
             TransportResponse transportResponse = transportServiceSoapClient.CreateTransaction(credentialsName, credentialsSiteID, credentialsKey, transportRequest);
             string transportKey = transportResponse.TransportKey;
             Console.WriteLine("TransportKey Received = {0}{1}", transportKey, Environment.NewLine);
 
-            //Initiate transaction with TransportKey
+            // Initiate transaction with TransportKey
             WebRequest webReq = WebRequest.Create($"http://{ipAddress}:8080/v2/pos?TransportKey={transportKey}&Format=XML");
             using (WebResponse webResp = webReq.GetResponse())
             {
                 using (Stream responseStream = webResp.GetResponseStream())
                 {
-                    //Validate XML to Genius XSD class
+                    // Validate XML to Genius XSD class
                     StreamReader streamReader = new StreamReader(responseStream);
                     XmlSerializer xmlSerializer = new XmlSerializer(typeof(TransactionResult));
                     TransactionResult transactionResult = (TransactionResult)xmlSerializer.Deserialize(streamReader);
                     Console.WriteLine("Transaction Result: {0}", transactionResult.Status);
                     Console.WriteLine("Amount: {0}", transactionResult.AmountApproved);
                     Console.WriteLine("AuthCode: {0}", transactionResult.AuthorizationCode);
-                    Console.WriteLine("Token: {0}",transactionResult.Token);
+                    Console.WriteLine("Token: {0}", transactionResult.Token);
                     Console.WriteLine("Account Number: {0}", transactionResult.AccountNumber);
                 }
             }
 
-            //Close application
+            // Close application
             Console.WriteLine("Press Any Key to Close");
             Console.ReadKey();
         }
