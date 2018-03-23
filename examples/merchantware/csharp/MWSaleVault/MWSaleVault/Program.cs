08555253d6dd7af3cdb5ca1c8e099616305a4bba
cayanlarkin <mlarkin@cayan.com>
2018-03-23 15:34:58 +0000

    StyleCop validation Changed outputs to multiple lines Cleaned some variable standards

--- examples/merchantware/csharp/MWSaleVault/MWSaleVault/Program.cs
+++ examples/merchantware/csharp/MWSaleVault/MWSaleVault/Program.cs
@@ -3,41 +3,42 @@ using MWSaleVault.MWCredit;
 
 namespace MWSaleVault
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //Create Soap Client
+            // Create Soap Client
             CreditSoapClient soapClient = new CreditSoapClient("CreditSoap");
-            //Create MerchantCredentails object
+            // Create MerchantCredentails object
             MerchantCredentials merchantCredentials = new MerchantCredentials
             {
                 MerchantName = "TEST MERCHANT",
                 MerchantSiteId = "XXXXXXXX",
                 MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
             };
-            //Create PaymentData object
+            // Create PaymentData object
             PaymentData paymentData = new PaymentData
             {
                 Source = "Vault",
-                VaultToken = "OTT_LT1KSFBNOZB381037S",
+                VaultToken = "OTT_LT1KSFBNOZB123456A"
             };
-            //Create SaleRequest Object
+            // Create SaleRequest Object
             SaleRequest saleRequest = new SaleRequest
             {
                 Amount = "1.01",
                 TaxAmount = "0.10",
-                InvoiceNumber = "1234",
+                InvoiceNumber = "INV1234",
                 CardAcceptorTerminalId = "01",
                 CustomerCode = "1234",
-                PurchaseOrderNumber = "1234",
+                PurchaseOrderNumber = "PO1234",
                 EnablePartialAuthorization = "true"
-
             };
-            //Run Sale
+            // Run Sale
             TransactionResponse45 saleResponse = soapClient.Sale(merchantCredentials, paymentData, saleRequest);
-            //Print Results
-            Console.WriteLine("Sale Response: {0}{3} Token: {1}{3} Amount: ${2}{3}", saleResponse.ApprovalStatus, saleResponse.Token, saleResponse.Amount, Environment.NewLine);
+            // Print Results
+            Console.WriteLine("Sale Response: {0}", saleResponse.ApprovalStatus);
+            Console.WriteLine("Amount: ${0}", saleResponse.Amount);
+            Console.WriteLine("Token: {0}", saleResponse.Token);
             Console.WriteLine("Press Any Key to Close");
             Console.ReadKey();
         }
